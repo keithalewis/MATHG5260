5299f26e2a02978539e5887151d2b6f149261cf2
jq2268 <jq2268@columbia.edu>
2017-09-19 20:24:35 -0400

    9.19 OH

--- sort.cpp
+++ sort.cpp
@@ -1,2 +1,26 @@
 // sort.cpp - std::sort from the STL
 //!!! Write xll_sort that calls std::sort and hook it up to XLL.SORT in Excel.
+/*
+AddIn xai_sort(
+	Function(XLL_LPOPER, L"?xll_sort", L"XLL.SORT")
+	.Arg(XLL_LPOPER, L"range", L"is a range.")
+	.FunctionHelp(L"Sorts the elements in the range in ascending order.")
+	.Category(CATEGORY)
+);
+LPOPER WINAPI xll_sort(LPOPER po)
+{
+#pragma XLLEXPORT
+	static OPER o;
+
+	try {
+		 std::sort(po->begin(), po->end());
+		 o.resize(1, std::distance(po->begin(), po->end()));
+		 std::copy(po->begin(), po->end(), o.begin());
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		o = OPER(xlerr::NA);
+	}
+
+	return &o;
+}*/
