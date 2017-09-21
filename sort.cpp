794f6c2006e707711aaceccc371b2080368b8386
sy2679 <31974606+sy2679@users.noreply.github.com>
2017-09-21 18:44:11 -0400

    Add files via upload

--- sort.cpp
+++ sort.cpp
@@ -8,7 +8,7 @@ using namespace xll;
 AddIn xai_sort(
 	Function(XLL_LPOPER, L"?xll_sort", L"XLL.SORT")
 	.Arg(XLL_LPOPER, L"range", L"is a range.")
-	.FunctionHelp(L"Remove adjacent duplicate entries from range.")
+	.FunctionHelp(L"Sort the selected list in ascending order")
 	.Category(CATEGORY)
 );
 LPOPER WINAPI xll_sort(LPOPER po)
@@ -17,13 +17,21 @@ LPOPER WINAPI xll_sort(LPOPER po)
 	static OPER o;
 
 	try {
-		std::unique(po->begin(), po->end());
+		// First get the length of the input
+		int leng = std::distance(po->begin(), po->end());
+
+		// Then sort the input with std
+		std::sort(po->begin(), po->end());
+
+		// Perform Resize
 		if (po->rows() == 1) {
-			o.resize(1, std::distance(po->begin(), po->end()));
+			o.resize(1, leng);
 		}
 		else {
-			o.resize(std::distance(po->begin(), po->end()), 1);
+			o.resize(leng, 1);
 		}
+		
+		// Output the sorted array
 		std::copy(po->begin(), po->end(), o.begin());
 	}
 	catch (const std::exception& ex) {
