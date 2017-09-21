3317ee978e852c43c66ba6ab5e245993a4ec3a17
鹏 章 <zhangpenglembert@hotmail.com>
2017-09-21 10:33:35 -0400

    HW1 for Peng Zhang pz2236

--- sort.cpp
+++ sort.cpp
@@ -1,2 +1,38 @@
 // sort.cpp - std::sort from the STL
 //!!! Write xll_sort that calls std::sort and hook it up to XLL.SORT in Excel.
+#include "G5260.h"
+
+using namespace xll;
+
+AddIn xai_sort(
+	Function(XLL_LPOPER, L"?xll_sort", L"XLL.SORT")
+	.Arg(XLL_LPOPER, L"range", L"is a range.")
+	.FunctionHelp(L"Sort numerical entries from selected range based on acsending order.")
+	.Category(CATEGORY)
+);
+LPOPER WINAPI xll_sort(LPOPER po)
+{
+#pragma XLLEXPORT
+	static OPER o;
+
+	try {
+		//Use std::sort from the STL to sort the selected range
+		std::sort(po->begin(), po->end());
+		//Get the length of the selected range
+		int size = std::distance(po->begin(), po->end());
+		//Resize the desingated column into appropriate length
+		if (po->rows() == 1) {
+			o.resize(1,size);
+		}
+		else {
+			o.resize(size, 1);
+		}
+		//Cope the sorted entries into the desingated column
+		std::copy(po->begin(), po->end(), o.begin());
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		o = OPER(xlerr::NA);
+	}
+	return &o;
+}
--- unique.cpp
+++ unique.cpp
@@ -15,9 +15,14 @@ LPOPER WINAPI xll_unique(LPOPER po)
     static OPER o;
 
     try {
-        auto e = std::unique(po->begin(), po->end());
-        o.resize(1, std::distance(po->begin(), e));
-        std::copy(po->begin(), e, o.begin());
+		auto e = std::unique(po->begin(), po->end());
+		if (po->rows() == 1) {
+			o.resize(1, std::distance(po->begin(), e));
+		}
+		else {
+			o.resize(std::distance(po->begin(), e), 1);
+		}
+		std::copy(po->begin(), e, o.begin());
     }
     catch (const std::exception& ex) {
         XLL_ERROR(ex.what());
