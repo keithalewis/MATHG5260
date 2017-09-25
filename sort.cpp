7d43cb41b5370895b29f10800cc1858acc99ef4c
ry2330 <ry2330@columbia.edu>
2017-09-25 15:32:05 -0400

    hw1-ry2330

--- sort.cpp
+++ sort.cpp
@@ -1,36 +1,34 @@
 // sort.cpp - std::sort from the STL
-#include <algorithm>
-#include <xutility>
+//!!! Write xll_sort that calls std::sort and hook it up to XLL.SORT in Excel.
 #include "G5260.h"
 
 using namespace xll;
 
 AddIn xai_sort(
-    Function(XLL_LPOPER, L"?xll_sort", L"XLL.SORT")
-    .Arg(XLL_LPOPER, L"range", L"is a range.")
-    .FunctionHelp(L"Sort entries from range.")
-    .Category(CATEGORY)
+	Function(XLL_LPOPER, L"?xll_sort", L"XLL.SORT")
+	.Arg(XLL_LPOPER, L"range", L"is a range.")
+	.FunctionHelp(L"Sort adjacent entries from range.")
+	.Category(CATEGORY)
 );
 LPOPER WINAPI xll_sort(LPOPER po)
 {
 #pragma XLLEXPORT
-    static OPER o;
+	static OPER o;
 
-    try {
-        std::sort(po->begin(), po->end());
-        if (po->rows() == 1) {
-            o.resize(1, po->columns());
-        }
-        else {
-            o.resize(po->rows(), po->columns());
-        }
-        std::copy(po->begin(), po->end(), o.begin());
-    }
-    catch (const std::exception& ex) {
-        XLL_ERROR(ex.what());
-        o = OPER(xlerr::NA);
-    }
+	try {
+		std::sort(po->begin(), po->end());
+		if (po->rows() == 1) {
+			o.resize(1, std::distance(po->begin(), po->end()));
+		}
+		else {
+			o.resize(std::distance(po->begin(), po->end()), 1);
+		}
+		std::copy(po->begin(), po->end(), o.begin());
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+		o = OPER(xlerr::NA);
+	}
 
-    return &o;
+	return &o;
 }
-
